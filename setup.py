1072c94a4437fa6d2a187213fd20cc7ecbef055b
Henrique <hentt30@gmail.com>
2020-10-09 13:27:43 -0400

    fixing install requires

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name="encut-opt",
       version="0.0.1",
       scripts=["encut_optimization.py"],
-      requires=["argparse", "pandas", "seaborn", "tqdm"],
+      install_requires=["argparse", "pandas", "seaborn", "tqdm"],
       entry_points="""
         [console_scripts]
         encut_optimization=encut_optimization:main
