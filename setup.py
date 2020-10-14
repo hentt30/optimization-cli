1d67dafc9da0c1ed1b2e486bd9b58cb90f78af79
Henrique <hentt30@gmail.com>
2020-10-14 06:39:47 -0400

    adding fields to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 from setuptools import setup
 setup(name="optimization",
       version="0.0.1",
+      author="Henrique Fernandes",
+      author_email="hentt30@gmail.com",
+      description ='Package for optimize parameters of vasp', 
+      license ='MIT', 
       scripts=[
           "optimization.py", "encut.py", "kpoints.py", "lattice_constant.py",
           "parser.py"
